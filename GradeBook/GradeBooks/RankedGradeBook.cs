6ca1b2bb1b692ce0207fc54acc6c331bda8e4bc3
srsadek <srsadek@users.noreply.github.com>
2018-03-09 15:23:12 -0500

    added GetLetterGrade methods logic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,37 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name):base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int minimumStudetRequiredToRank = 5;
+
+            if (this.Students.Count < minimumStudetRequiredToRank)
+            {
+                throw new InvalidOperationException("Ranked Grading requires a minimum of 5 students to work");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grades[threshold-1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if(grades[threshold*2 - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
