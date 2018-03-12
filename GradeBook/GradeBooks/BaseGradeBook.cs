d00982a1825b4cb82e300cd40048e8c1950dfbdf
srsadek <srsadek@users.noreply.github.com>
2018-03-12 10:18:27 -0400

    variable missmatch in constactor of BaseGradeBook, fixed that

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -19,7 +19,7 @@ namespace GradeBook.GradeBooks
         public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
-            this.IsWeighted = IsWeighted;
+            this.IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
